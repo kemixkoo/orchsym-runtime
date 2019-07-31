1f7689f146f532b27ebc760a2218c95cb9f6f121
余露 <lu.yu@baishancloud.com>
2019-07-31 10:22:18 +0800

    文件大小写

--- orchsym/orchsym-fe/src/components/Application/appList.js
+++ orchsym/orchsym-fe/src/components/Application/appList.js
@@ -6,7 +6,6 @@ import ApplicationSearch from './ApplicationSearch';
 import EditApplication from './EditApplication';
 import SortApplication from './SortApplication';
 import SaveTemp from './SaveTemp';
-import LogList from '../logList';
 
 export default class AppList extends PureComponent {
   state = {
@@ -180,9 +179,9 @@ export default class AppList extends PureComponent {
           </div>
           <p className={styles.cardTime}>5小时前</p>
           {(isError) ? (
-            <Dropdown overlay={<LogList />}>
-              <span className={styles.triangle} />
-            </Dropdown>
+            // <Dropdown overlay={<LogList />}>
+            <span className={styles.triangle} />
+            // </Dropdown>
           ) : (null)}
         </Card>
       </Col>
