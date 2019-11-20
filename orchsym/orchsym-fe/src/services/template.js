07f25d326e4fd590b122486eb77a201185abae42
王丹 <31149651+ortion@users.noreply.github.com>
2019-11-20 18:58:16 +0800

    下载测试

--- orchsym/orchsym-fe/src/services/template.js
+++ orchsym/orchsym-fe/src/services/template.js
@@ -1,5 +1,11 @@
 import request from '@/utils/request';
 
 export async function downloadApplication(appId) {
-  return request(`/studio/orchsym-api/template/app/${appId}/download`, { getResponse: true });
+  return request(`/studio/orchsym-api/template/app/${appId}/download`, { getResponse: true })
+    .then(({ data, response }) => {
+      console.log(response)
+      const fileName = response.headers['Content-Disposition'].split('filename=')[1]
+      console.log(fileName)
+      localStorage.setItem('fileName', fileName);
+    });
 }
